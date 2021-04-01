8a8230c038307bc7025c23a576592d12bd7dd4d5
Denisuktc <80036970+Denisuktc@users.noreply.github.com>
2021-04-01 12:37:21 +0300

    Create Zadacha3.java

---
+++ Zadacha3.java
@@ -0,0 +1,16 @@
+public class Main {
+  
+  public static void main(String[] args){
+    
+    Scanner zadacha = new Scanner(System.in);
+    double a,b,c;
+    
+    a = Double.parseDouble(zadacha.nextLine());
+    b = Double.parseDouble(zadacha.nextLine());
+    c = Double.parseDouble(zadacha.nextLine());
+    
+    float S = ((a.2)+(b.2)).c;
+    float V = a.b.c;
+    
+    System.out.println(S);
+    System.out.print(V);
